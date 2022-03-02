7156c50acb105379646b7e856da3352c2dbd71da
unknown <alexerdeader@gmail.com>
2022-03-02 14:11:06 +0300

    add header-shadow after scrolling, custom cursor, hover,active effects of buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,12 +4,16 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    "(window:click)": "onClick()"
+  }
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isMenu = true;
   isShow = true;
   isShown = false;
+  h = document.querySelector('.header');
 
   toggleDisplay() {
     this.isShow = !this.isShow;
@@ -27,7 +31,30 @@ export class HeaderComponent {
     }
   }
 
-  toggleUser() {
+  toggleUser(event: Event) {
+    event.stopPropagation();
     this.isMenu = !this.isMenu;
+  }
+
+  onClick() {
+    this.isMenu = true;
+  }
+  ngOnInit(): void {
+
+    window.onload = () => {
+      onScroll();
+    };
+
+    function onScroll() {
+      window.addEventListener("scroll", callbackFunc);
+      function callbackFunc() {
+        let y = window.pageYOffset;
+        if (y > 10) {
+          document.querySelector('.header')?.classList.add("scroll");
+        } else {
+          document.querySelector('.header')?.classList.remove("scroll");
+        }
+      }
+    }
   }
 }
